081d9159b0e050b544cc2c9b7d2f8656a53fb53e
Miku <100752586+MikuNakano02@users.noreply.github.com>
2022-06-07 15:54:52 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hey💖 hey💖 , Hello💖 M.sender.username \n\n_senpai💖 ℍ𝕠𝕨 𝕒𝕣𝕖 𝕪𝕠𝕦💖 𝕋𝕪𝕡𝕖 ${this.client.config.prefix}help 𝕥𝕠 𝕤𝕖𝕖 𝕞𝕪 𝕔𝕒𝕞𝕞𝕒𝕟𝕕'𝕤 𝕝𝕚𝕤𝕥 \n\n`,
+				caption: `ᴍᴏꜱʜɪ ᴍᴏꜱʜɪ  M.sender.username \n\n_senpai💖 ℍ𝕠𝕨 𝕒𝕣𝕖 𝕪𝕠𝕦💖 𝕋𝕪𝕡𝕖 ${this.client.config.prefix}help 𝕥𝕠 𝕤𝕖𝕖 𝕞𝕪 𝕔𝕒𝕞𝕞𝕒𝕟𝕕'𝕤 𝕝𝕚𝕤𝕥 \n\n`,
 			}
 		);
 	};
